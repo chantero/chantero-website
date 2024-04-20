69bcb672623c2cb81240f51b41a4e6cc583b95ec
Chantero <dev@chantero.com>
2024-04-20 21:02:29 +0800

    amend some typo
    unit per price - to price per unit

--- price-calculator/script.js
+++ price-calculator/script.js
@@ -10,9 +10,9 @@ function calculate() {
   const unitPerPrice2 = isNaN(price2) || isNaN(unit2) ? '' : (price2 / unit2).toFixed(2);
   const unitPerPrice3 = isNaN(price3) || isNaN(unit3) ? '' : (price3 / unit3).toFixed(2);
 
-  document.getElementById('results1').innerHTML = `<strong>Unit per Price:</strong> ${unitPerPrice1}`;
-  document.getElementById('results2').innerHTML = `<strong>Unit per Price:</strong> ${unitPerPrice2}`;
-  document.getElementById('results3').innerHTML = `<strong>Unit per Price:</strong> ${unitPerPrice3}`;
+  document.getElementById('results1').innerHTML = `<strong>Price per Unit:</strong> ${unitPerPrice1}`;
+  document.getElementById('results2').innerHTML = `<strong>Price per Unit:</strong> ${unitPerPrice2}`;
+  document.getElementById('results3').innerHTML = `<strong>Price per Unit:</strong> ${unitPerPrice3}`;
 
   const minUnitPerPrice = Math.min(unitPerPrice1 || Infinity, unitPerPrice2 || Infinity, unitPerPrice3 || Infinity);
 
